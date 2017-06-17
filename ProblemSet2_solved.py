edd663ac6c7b74805cf1ec7b142e445b0e7080a0
Olga <charminola@yahoo.com>
2017-06-17 15:48:49 +0300

    Update ProblemSet2_solved.py
    the whole problem set is solved

--- ProblemSet2_solved.py
+++ ProblemSet2_solved.py
@@ -313,8 +313,10 @@ hourly_temp = [40.0, 39.0, 37.0, 34.0, 33.0, 34.0, 36.0, 37.0, 38.0, 39.0, \
                36.0, 35.0, 33.0, 32.0]
 #%%
 def problem2_8(temp_list):
-    pass # replace this pass (a do-nothing) statement with your code
-        
+    print ("Average: ", sum(temp_list)/len(temp_list))
+    print ("High: ", max(temp_list))
+    print ("Low: ", min(temp_list))
+     
     
 #%%
 """
